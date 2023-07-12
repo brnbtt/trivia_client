e5bbad6aa6e75020a62134f934246f7c861a14d0
Bruno <brnbtt@icloud.com>
2023-07-12 19:38:22 -0300

    Added background and improved design

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
@@ -13,9 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Trivia'),
@@ -49,53 +51,149 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   late final IO.Socket socket = IO.io('http://localhost:3333');
   bool _showNameInput = true;
-  String _name = '';
+  late TextEditingController _nameController;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat();
+  }
 
   void _submitName() {
-    socket.emit('nameEntered', _name);
+    socket.emit('nameEntered', _nameController.text);
     setState(() {
       _showNameInput = false;
     });
   }
 
   @override
+  void dispose() {
+    socket.dispose();
+    _nameController.dispose();
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  bool _isSubmitButtonEnabled() {
+    return _nameController.text.isNotEmpty;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: _showNameInput
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 250,
-                    child: TextField(
-                      onChanged: (value) {
-                        setState(() {
-                          _name = value;
-                        });
-                      },
-                      keyboardType: TextInputType.name,
-                      autofocus: true,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your name',
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          AnimatedBuilder(
+            animation: _animationController,
+            builder: (context, child) {
+              final color1 = HSVColor.fromAHSV(
+                1.0,
+                _animationController.value * 360,
+                1.0,
+                1.0,
+              ).toColor();
+              final color2 = HSVColor.fromAHSV(
+                1.0,
+                (_animationController.value + 0.5) % 1.0 * 360,
+                1.0,
+                1.0,
+              ).toColor();
+
+              return Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [color1, color2],
+                  ),
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 8.0, sigmaY: 8.0),
+                  child: child,
+                ),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.5),
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  widget.title,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                _showNameInput
+                    ? Card(
+                        color: Colors.white.withOpacity(0.6),
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: SizedBox(
+                                  width: 250,
+                                  child: Column(
+                                    children: [
+                                      TextField(
+                                        controller: _nameController,
+                                        onChanged: (_) {
+                                          setState(() {});
+                                        },
+                                        keyboardType: TextInputType.name,
+                                        autofocus: true,
+                                        decoration: const InputDecoration(
+                                          border: OutlineInputBorder(),
+                                          hintText: 'Enter your name',
+                                        ),
+                                      ),
+                                      const SizedBox(height: 16),
+                                      ElevatedButton(
+                                        onPressed: _isSubmitButtonEnabled()
+                                            ? _submitName
+                                            : null,
+                                        child: const Text('Submit'),
+                                      ),
+                                    ],
+                                  )),
+                            ),
+                          ],
+                        ),
+                      )
+                    : Card(
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        margin: const EdgeInsets.all(16),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: MyButton(socket: socket),
+                        ),
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 16, width: 50),
-                  ElevatedButton(
-                    onPressed: _name.isNotEmpty ? _submitName : null,
-                    child: const Text('Submit'),
-                  ),
-                ],
-              )
-            : MyButton(socket: socket),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
